97bea6cb25d7b6bdad16c6445fad70e09bb02be2
Aniket_IN <aniket.das.in@gmail.com>
2021-09-29 18:04:36 +0530

    Laravel Websockets Http Working

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -38,8 +38,9 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: 'myKey',
+    key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
-        wsPort: 6001,
-        disableStats: true,
+    wsPort: 6001,
+    forceTLS: false,
+    disableStats: true,
 });
